8662e1cf413e3a276c197c4c15e88fc8a9554e18
Daniel Dushev <danieldyshew@gmail.com>
2023-08-09 16:02:50 +0300

    Added static files; implemented catalog and details;

--- JavaScript Back-End/1.Express-js-and-templating-workshop/AirBnB-workshop/controllers/catalogController.js
+++ JavaScript Back-End/1.Express-js-and-templating-workshop/AirBnB-workshop/controllers/catalogController.js
@@ -1,15 +1,30 @@
+const { getAll, getById } = require('../services/accomodationService');
+
 const router = require('express').Router();
 
 router.get('/', (req,res) => {
+    const rooms = getAll();
     res.render('catalog', {
-        title: 'All accomodations'
+        title: 'All accomodations',
+        rooms
     });
 })
 
 router.get('/:id', (req,res) => {
-    res.render('details', {
-        title: 'Accomodation details'
-    });
+    const id = req.params.id;
+    const room = getById(id);
+    if(room) {
+        res.render('details', {
+            title: 'Accomodation details',
+            room
+        });
+    }else {
+        res.render('accNotFound' , {
+            title: 'Accommodation not found',
+            id
+        })
+    }
+    
 })
 
 module.exports = router;
---
+++ JavaScript Back-End/1.Express-js-and-templating-workshop/AirBnB-workshop/services/accomodationService.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const filename = './models/data.json';
+const data = JSON.parse(fs.readFileSync(filename));
+
+async function persist() {  
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, JSON.stringify(data), (err) => {
+            if (err == null) {
+                resolve();
+            }else {
+                reject(err);
+            }
+        });
+    });
+}
+
+function getAll() {
+    return data;
+}
+
+function getById(id) {
+    return data.find(i => i.id == id);
+}
+
+module.exports = {
+    getAll,
+    getById
+}
