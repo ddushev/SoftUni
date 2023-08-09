273c1f09c8ff7ad44d1e7a1e2f9b6a98f570b23e
Daniel Dushev <danieldyshew@gmail.com>
2023-08-09 17:22:23 +0300

    Layout updated + added create functionality

--- JavaScript Back-End/1.Express-js-and-templating-workshop/AirBnB-workshop/controllers/createController.js
+++ JavaScript Back-End/1.Express-js-and-templating-workshop/AirBnB-workshop/controllers/createController.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
+const { create } = require('../services/accomodationService');
+
 
 router.get('/', (req, res) => {
     res.render('create', {
         title: 'Host new accommodation'
     })
+})
+
+router.post('/', async (req, res) => {
+
+    try {
+        const result = await create(req.body);
+        res.redirect('/catalog/' + result.id);
+    } catch (error) {
+        res.render('/create', {
+            title: 'Request Error'
+        })
+    }
+
 })
 
 module.exports = router;
--- JavaScript Back-End/1.Express-js-and-templating-workshop/AirBnB-workshop/services/accomodationService.js
+++ JavaScript Back-End/1.Express-js-and-templating-workshop/AirBnB-workshop/services/accomodationService.js
@@ -5,7 +5,7 @@ const data = JSON.parse(fs.readFileSync(filename));
 
 async function persist() {  
     return new Promise((resolve, reject) => {
-        fs.writeFile(filename, JSON.stringify(data), (err) => {
+        fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
             if (err == null) {
                 resolve();
             }else {
@@ -23,7 +23,28 @@ function getById(id) {
     return data.find(i => i.id == id);
 }
 
+async function create(roomData) {
+    console.log(roomData);
+    const room = {
+        id: getId(),
+        name: roomData.name,
+        city: roomData.city,
+        imgUrl: roomData.imgUrl,
+        beds: Number(roomData.beds),
+        price: Number(roomData.price),
+        description: roomData.description
+    }
+    data.push(room);
+    await persist();
+    return room;
+}
+
+function getId() {
+    return ('000000' + (Math.random() * 999999 | 0).toString(16)).slice(-6);
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    create
 }
