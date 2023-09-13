e796dba78a9182e96ad680815b9ee1a1b773461b
Daniel Dushev <danieldyshew@gmail.com>
2023-09-13 11:16:03 +0300

    Show edit/delete buttons only for users

--- JavaScript Back-End/3.Session-and-authenticating/Cubicle-Workshop-Part-3/controllers/catalogController.js
+++ JavaScript Back-End/3.Session-and-authenticating/Cubicle-Workshop-Part-3/controllers/catalogController.js
@@ -5,6 +5,7 @@ const catalogController = require('express').Router();
 catalogController.get('/:id', async (req, res) => {
     try {
         const cube = await getDataById(req.params.id);
+        cube.isOwner = req.user._id == cube.creatorId;
         res.render('details', {
             title: `Cube ${req.params.id}`,
             cube
