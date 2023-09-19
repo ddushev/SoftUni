c0f3ba6f533eb2c2905162c1683c2222592ab0c9
Daniel Dushev <danieldyshew@gmail.com>
2023-09-19 13:13:09 +0300

    Add validation to User model

--- JavaScript Back-End/4.Validations-and-error-handling/Cubicle-Workshop-Part-4/models/User.js
+++ JavaScript Back-End/4.Validations-and-error-handling/Cubicle-Workshop-Part-4/models/User.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    username: { 
+        type: String, 
+        required: true, 
+        minLength: [5, 'Min length is 5 characters!'], 
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z0-9]+$/g.test(v)
+            },
+            message: props => `${props.value} contains not allowed characters`
+        }
+    },
     hashedPassword: { type: String, required: true }
 });
 
