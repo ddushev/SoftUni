6a3ec2ba9cfbba82c93f5b8b9703b431d94f1011
Daniel Dushev <danieldyshew@gmail.com>
2023-09-20 18:01:26 +0300

    Add validation for creating items

--- JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/controllers/createController.js
+++ JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/controllers/createController.js
@@ -11,6 +11,29 @@ createController.get('/item', (req, res) => {
 
 createController.post('/item', async (req, res) => {
     try {
+        
+        const errors = [];
+
+        if (req.body.name.length < 2) {
+            errors.push('Name should be atleast 2 characters long!');
+        }
+
+        if (!/^(https?:\/\/)/.test(req.body.imageUrl)) {
+            errors.push('ImageUrl should start with http:// or https://!');
+        }
+
+        if (Number(req.body.price) <= 0) {
+            errors.push('Price should be positive number!');
+        }
+
+        if (req.body.description.length < 10) {
+            errors.push('Description should be atleast 10 characters long!');
+        }
+
+        if (errors.length > 0) {
+            throw errors;
+        }
+
         await createData(req.body, req.user._id);
         res.redirect('/');
     } catch (error) {
