69c1b59a74bf80c6be6773692b276ce2d144eb9f
Daniel Dushev <danieldyshew@gmail.com>
2023-09-20 16:21:36 +0300

    Add logout func

--- JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/config/routes.js
+++ JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/config/routes.js
@@ -1,11 +1,11 @@
 const homeController = require("../controllers/homeController");
 const defaultController = require("../controllers/defaultController");
 const authController = require("../controllers/authController");
+const logoutController = require("../controllers/logoutController");
 // const attachController = require("../controllers/attachController");
 // const catalogController = require("../controllers/catalogController");
 // const createController = require("../controllers/createController");
 // const cubeController = require("../controllers/cubeController");
-// const logoutController = require("../controllers/logoutController");
 const { hasUser, isGuest } = require("../middlewares/guards");
 
 // TODO: Require Controllers...
@@ -13,10 +13,10 @@ const { hasUser, isGuest } = require("../middlewares/guards");
 module.exports = (app) => {
     app.use(homeController);
     app.use('/auth', isGuest, authController);
+    app.use('/logout', hasUser, logoutController);
     // app.use('/details', catalogController);
     // app.use('/create', hasUser, createController);
     // app.use('/attach', hasUser, attachController);
-    // app.use('/logout', hasUser, logoutController);
     // app.use('/cube', hasUser, cubeController);
 
     app.all('*', defaultController);
