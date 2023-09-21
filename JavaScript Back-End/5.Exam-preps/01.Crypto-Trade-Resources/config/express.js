0291f8b68ebc6b3d778bdc2e7f25955df5ec63a6
Daniel Dushev <danieldyshew@gmail.com>
2023-09-21 16:06:50 +0300

    Add edit functionality

--- JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/config/express.js
+++ JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/config/express.js
@@ -3,14 +3,15 @@ const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const auth = require('../middlewares/auth');
+const Handlebars = require('handlebars');
 
 module.exports = (app) => {
     
-    //TODO: Setup the view engine
+    //Setup the view engine
     app.engine('.hbs', hbs.engine({extname: '.hbs'}));
     app.set('view engine', '.hbs');
 
-    //TODO: Setup the body parser
+    //Setup the body parser
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json());
 
@@ -20,6 +21,11 @@ module.exports = (app) => {
     //Middleware checking for verified user
     app.use(auth());
 
-    //TODO: Setup the static files
+    //Setup the static files
     app.use('/static', express.static('static'));
+
+    //Add Handlebars custom helper for selected options
+    Handlebars.registerHelper('isSelected', function(value, selectedValue) {
+        return value == selectedValue ? 'selected' : '';
+    })
 };
--- JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/controllers/itemController.js
+++ JavaScript Back-End/5.Exam-preps/01.Crypto-Trade-Resources/controllers/itemController.js
@@ -1,5 +1,6 @@
 const { createData, getDataById, editData } = require('../services/itemService');
 const errorParser = require('../utils/errorParser');
+const validationChecker = require('../utils/validationChecker');
 
 const itemController = require('express').Router();
 
@@ -13,29 +14,7 @@ itemController.get('/create', (req, res) => {
 
 itemController.post('/create', async (req, res) => {
     try {
-        
-        const errors = [];
-
-        if (req.body.name.length < 2) {
-            errors.push('Name should be atleast 2 characters long!');
-        }
-
-        if (!/^(https?:\/\/)/.test(req.body.imageUrl)) {
-            errors.push('ImageUrl should start with http:// or https://!');
-        }
-
-        if (Number(req.body.price) <= 0) {
-            errors.push('Price should be positive number!');
-        }
-
-        if (req.body.description.length < 10) {
-            errors.push('Description should be atleast 10 characters long!');
-        }
-
-        if (errors.length > 0) {
-            throw errors;
-        }
-
+        validationChecker(req);
         await createData(req.body, req.user._id);
         res.redirect('/');
     } catch (error) {
@@ -46,52 +25,38 @@ itemController.post('/create', async (req, res) => {
     }
 });
 
-//Buy functionality
-itemController.get('/:id/buy', async (req, res) => {
-
-    try {
-        const item = await getDataById(req.params.id);
-        if (item.creatorId == req.user._id) {
-            throw new Error('You can\'t buy your own coins!');
-        }
-        item.userCollection.push(req.user._id);
-        await editData(item, req.params.id);
-        res.redirect(`/catalog/${req.params.id}/details`)
-    } catch (error) {
-        res.render('404', {
-            title: 'Unable to purchase',
-            error: errorParser(error)
-        });
-    }
-});
-
-
-
 //Edit functionality
 itemController.get('/:id/edit', async (req, res) => {
     try {
         const item = await getDataById(req.params.id);
         if (item.creatorId != req.user._id) {
-            res.redirect('/404');
+            throw new Error('You must be creator to edit an item!')
         }
         res.render('edit', {
             title: `Edit item`,
             item
         });
     } catch (error) {
-        console.log(error.message);
-        res.render('404');
+        res.render('404', {
+            title: 'Unable to open edit page',
+            error: errorParser(error)
+        });
     }
 
 });
 
 itemController.post('/:id/edit', async (req, res) => {
     try {
+        validationChecker(req);
         const item = await editData(req.body, req.params.id);
-        res.redirect(`/details/${item._id}`);
+        res.redirect(`/catalog/${item._id}/details`);
     } catch (error) {
-        console.log(error.message);
-        res.render('404');
+        const item = await getDataById(req.params.id);
+        res.render('edit', {
+            title: 'Unable to edit item',
+            item,
+            error: errorParser(error)
+        });
     }
 
 });
@@ -122,8 +87,25 @@ itemController.post('/:id/delete' , async (req, res) => {
         console.log(error.message);
         res.render('404');
     }
-})
+});
 
+//Buy functionality
+itemController.get('/:id/buy', async (req, res) => {
 
+    try {
+        const item = await getDataById(req.params.id);
+        if (item.creatorId == req.user._id) {
+            throw new Error('You can\'t buy your own coins!');
+        }
+        item.userCollection.push(req.user._id);
+        await editData(item, req.params.id);
+        res.redirect(`/catalog/${req.params.id}/details`)
+    } catch (error) {
+        res.render('404', {
+            title: 'Unable to purchase',
+            error: errorParser(error)
+        });
+    }
+});
 
 module.exports = itemController;
