f3f499406873bdfb2d0a32c924ecf604a4f550d1
Daniel Dushev <danieldyshew@gmail.com>
2023-09-27 18:53:34 +0300

    Add Description component

--- React/01-introduction-to-react-and-jsx/src/App.js
+++ React/01-introduction-to-react-and-jsx/src/App.js
@@ -1,3 +1,4 @@
+import Description from "./components/Description";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 
@@ -9,26 +10,7 @@ function App() {
       <Header />
       
       <div className="container">
-        {/* <!-- Start: Desc --> */}
-        <div className="row me-row content-ct">
-          <h2 className="row-title">Why This Event Is Super Cool?</h2>
-          <div className="col-md-4 feature">
-            <span className="ti-ticket"></span>
-            <h3>Buy Early Bird Tickets</h3>
-            <p>Super cool discounts if you purchase early bird tickets now. Limited tickets available with some awesome perks and goodies!</p>
-          </div>
-          <div className="col-md-4 feature">
-            <span className="ti-microphone"></span>
-            <h3>Expert Speakers</h3>
-            <p>Best in class expert speakers who have extensive knowledge of the topic. All speakers are curated by the panel of experts.</p>
-          </div>
-          <div className="col-md-4 feature">
-            <span className="ti-world"></span>
-            <h3>People around the globe!</h3>
-            <p>Meet the people attending this event around the globe. This will be the best opportunity to meet and greet people from your industry.</p>
-          </div>
-        </div>
-        {/* <!-- End: Desc --> */}
+      <Description />
 
         {/* <!-- Start: Speakers --> */}
         <div className="row me-row content-ct speaker" id="speakers">
