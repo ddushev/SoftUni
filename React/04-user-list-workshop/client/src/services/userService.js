79cbfa05899b291c248ce6eabc07f7a725c74b2d
Daniel Dushev <danieldyshew@gmail.com>
2023-10-02 16:10:24 +0300

    Add show and hide user info

--- React/04-user-list-workshop/client/src/services/userService.js
+++ React/04-user-list-workshop/client/src/services/userService.js
@@ -4,3 +4,8 @@ export async function getUsers() {
     const resp = await fetch(baseUrl);
     return resp.json();
 }
+
+export async function getUser(id) {
+    const resp = await fetch(`${baseUrl}/${id}`);
+    return resp.json();
+}
