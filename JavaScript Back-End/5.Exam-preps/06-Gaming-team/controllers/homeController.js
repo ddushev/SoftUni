609bc802fcda36ecc76bc9254fe17c38dbd733ed
Daniel Dushev <danieldyshew@gmail.com>
2023-10-13 15:00:37 +0300

    removed fetch items for homeController

--- JavaScript Back-End/5.Exam-preps/06-Gaming-team/controllers/homeController.js
+++ JavaScript Back-End/5.Exam-preps/06-Gaming-team/controllers/homeController.js
@@ -1,13 +1,9 @@
 const homeController = require('express').Router();
-const { getData } = require('../services/itemService');
 
 
 homeController.get('/', async (req, res) => {
-    //TODO: If home shows some items
-    const items = (await getData()).slice(0,3);
     res.render('home', {
         title: 'Homepage',
-        items
     });
 });
 
