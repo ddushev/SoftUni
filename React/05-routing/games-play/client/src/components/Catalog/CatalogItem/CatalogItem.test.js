48b628b89fa6bdf9a65bed0fba08402edc3508c6
Daniel Dushev <danieldyshew@gmail.com>
2023-10-16 15:05:41 +0300

    Add click on details test

--- React/05-routing/games-play/client/src/components/Catalog/CatalogItem/CatalogItem.test.js
+++ React/05-routing/games-play/client/src/components/Catalog/CatalogItem/CatalogItem.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import CatalogItem from "./CatalogItem";
 import { BrowserRouter } from "react-router-dom";
 import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
 
 
 describe('Catalog Item Component', () => {
@@ -14,6 +15,21 @@ describe('Catalog Item Component', () => {
         );
 
         expect(screen.queryByText(title)).toBeInTheDocument();
+    });
+
+    test('Click on details', async () => {
+        global.window = { location: { pathname: null } };
+        const itemId = 'id';
+
+        render(
+            <BrowserRouter >
+                <CatalogItem _id={itemId} />
+            </BrowserRouter>
+        );
+
+        await userEvent.click(screen.queryByText('Details'));
+
+        expect(global.window.location.pathname).toContain(`/details/${itemId}`);
     });
 
 });
