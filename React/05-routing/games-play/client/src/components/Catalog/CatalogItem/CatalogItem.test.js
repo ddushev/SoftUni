58fe0c93b57919f1edc5b7f8b958b805852c5aa4
Daniel Dushev <danieldyshew@gmail.com>
2023-10-16 13:55:36 +0300

    Add Jest tests as new branch

---
+++ React/05-routing/games-play/client/src/components/Catalog/CatalogItem/CatalogItem.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import CatalogItem from "./CatalogItem";
+import { BrowserRouter } from "react-router-dom";
+
+
+describe('Catalog Item Component', () => {
+    test('Show title', () => {
+        const title = 'Test title';
+        render(
+            <BrowserRouter >
+                <CatalogItem title={title} />
+            </BrowserRouter>
+        );
+
+        expect(screen.queryByText(title)).toBeInTheDocument();
+    });
+
+});
