01bc4e83278a6c455bddeb0a970cfc18a9a7c1b8
Daniel Dushev <danieldyshew@gmail.com>
2023-10-16 14:10:51 +0300

    Fixed Jest tests with React testing library with Vite

--- React/05-routing/games-play/client/src/components/Catalog/CatalogItem/CatalogItem.test.js
+++ React/05-routing/games-play/client/src/components/Catalog/CatalogItem/CatalogItem.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import CatalogItem from "./CatalogItem";
 import { BrowserRouter } from "react-router-dom";
+import '@testing-library/jest-dom';
 
 
 describe('Catalog Item Component', () => {
