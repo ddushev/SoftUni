38b81ce9bc378bdc5c2526b64610d9ee5f291b49
Daniel Dushev <danieldyshew@gmail.com>
2024-01-24 14:46:21 +0200

    main & posts & themes. add components

--- Angular/01.Components/app/src/app/components/main/main.component.ts
+++ Angular/01.Components/app/src/app/components/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ThemesComponent } from './themes/themes.component';
+import { PostsComponent } from './posts/posts.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ThemesComponent, PostsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
