75189862186c25e348907625e2b3e6268cafc240
Daniel Dushev <danieldyshew@gmail.com>
2024-01-25 21:10:27 +0200

    themes-and-post. update test imports

--- Angular/02.modules-routing/client/src/app/components/themes-and-posts/themes-and-posts.component.spec.ts
+++ Angular/02.modules-routing/client/src/app/components/themes-and-posts/themes-and-posts.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import { ThemesAndPosts } from './themes-and-posts.component';
 
-describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+describe('ThemesAndPosts', () => {
+  let component: ThemesAndPosts;
+  let fixture: ComponentFixture<ThemesAndPosts>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainComponent]
+      imports: [ThemesAndPosts]
     })
     .compileComponents();
-    
-    fixture = TestBed.createComponent(MainComponent);
+
+    fixture = TestBed.createComponent(ThemesAndPosts);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
