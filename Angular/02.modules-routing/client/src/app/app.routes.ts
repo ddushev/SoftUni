90799be1bc07246e8914f986b9a4592f7deb7f11
Daniel Dushev <danieldyshew@gmail.com>
2024-01-25 21:34:13 +0200

    login & register. add components with routing

--- Angular/02.modules-routing/client/src/app/app.routes.ts
+++ Angular/02.modules-routing/client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+];
