379fd545cb6b632d7140a3ac7a6c50c6b1a09010
Daniel Dushev <danieldyshew@gmail.com>
2024-01-26 22:31:50 +0200

    themes-and-posts.  themes if user is logged in

--- Angular/02.modules-routing/client/src/app/components/themes-and-posts/themes-and-posts.component.ts
+++ Angular/02.modules-routing/client/src/app/components/themes-and-posts/themes-and-posts.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { ThemesComponent } from './themes/themes.component';
 import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from '../home/home.component';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ThemesComponent, PostsComponent],
+  imports: [ThemesComponent, PostsComponent, HomeComponent],
   templateUrl: './themes-and-posts.component.html',
   styleUrl: './themes-and-posts.component.scss'
 })
 export class ThemesAndPosts {
-
+  constructor(public userService: UserService) {}
 }
