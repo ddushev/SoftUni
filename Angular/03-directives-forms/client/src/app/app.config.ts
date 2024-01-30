b4edbde1ff096fccce5db697f13d9f1e0be5c216
Daniel Dushev <danieldyshew@gmail.com>
2024-01-30 18:34:12 +0200

    app.config. add pre-loading for all lazy loaded modules

--- Angular/03-directives-forms/client/src/app/app.config.ts
+++ Angular/03-directives-forms/client/src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch(), withInterceptors([authInterceptor]))]
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideHttpClient(withFetch(), withInterceptors([authInterceptor]))]
 };
