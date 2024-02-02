1f25161a9de2462376b69fc8690f6bf3898d7918
Daniel Dushev <danieldyshew@gmail.com>
2024-02-02 17:02:22 +0200

    packages. add store-devtools

--- Angular/04-pipes-interceptors-subjects/client/src/app/app.config.ts
+++ Angular/04-pipes-interceptors-subjects/client/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideHttpClient(withFetch(), withInterceptors([authInterceptor])), provideStore()]
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideHttpClient(withFetch(), withInterceptors([authInterceptor])), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
