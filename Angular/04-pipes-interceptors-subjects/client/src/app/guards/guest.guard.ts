838e2852f993c4c7f6cfd46da062a5d8a638cc65
Daniel Dushev <danieldyshew@gmail.com>
2024-02-02 10:00:48 +0200

    guards. fix guards

--- Angular/04-pipes-interceptors-subjects/client/src/app/guards/guest.guard.ts
+++ Angular/04-pipes-interceptors-subjects/client/src/app/guards/guest.guard.ts
@@ -1,12 +1,24 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../services/user/user.service';
 import { inject } from '@angular/core';
+import { asyncScheduler, catchError, map, scheduled, take } from 'rxjs';
+
 
 export const guestGuard: CanActivateFn = (_route, _state) => {
   const router = inject(Router);
   const userService = inject(UserService);
-  if (userService.user$) {
-    return router.navigate(['/themes']);
-  }
-  return true;
+
+  return userService.verifyUser().pipe(
+    take(1),
+    map((user) => {
+      if (user) {
+        router.navigate(["themes"]);
+        return false;
+      }
+      return true;
+    }),
+    catchError(() => {
+      return scheduled([true], asyncScheduler);
+    })
+  );
 };
--- Angular/04-pipes-interceptors-subjects/client/src/app/guards/user.guard.ts
+++ Angular/04-pipes-interceptors-subjects/client/src/app/guards/user.guard.ts
@@ -1,27 +1,25 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../services/user/user.service';
-import { catchError, map, of, take } from 'rxjs';
+import { asyncScheduler, catchError, map, scheduled, take } from 'rxjs';
+
 
 export const userGuard: CanActivateFn = (_route, _state) => {
   const router = inject(Router);
   const userService = inject(UserService);
-  if (!userService.user$) {
-    return router.navigate(['/login']);
-  }
-  return true;
-  // return userService.verifyUser().pipe(
-  //   take(1),
-  //   map((user) => {
-  //     if (!user) {
-  //       router.navigate(["login"]);
-  //       return false;
-  //     }
-  //     return true;
-  //   }),
-  //   catchError(() => {
-  //     router.navigate(["login"]);
-  //     return of(false);
-  //   })
-  // );
+
+  return userService.verifyUser().pipe(
+    take(1),
+    map((user) => {
+      if (!user) {
+        router.navigate(["login"]);
+        return false;
+      }
+      return true;
+    }),
+    catchError(() => {
+      router.navigate(["login"]);
+      return scheduled([false], asyncScheduler);
+    })
+  );
 };
