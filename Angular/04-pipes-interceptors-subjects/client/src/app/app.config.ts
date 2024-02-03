d7d7641fd6dcca9df5707019fdb1bb145c26ee71
Daniel Dushev <danieldyshew@gmail.com>
2024-02-03 16:53:43 +0200

    packages. add ngrx effects

--- Angular/04-pipes-interceptors-subjects/client/src/app/app.config.ts
+++ Angular/04-pipes-interceptors-subjects/client/src/app/app.config.ts
@@ -6,7 +6,8 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideHttpClient(withFetch(), withInterceptors([authInterceptor])), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideHttpClient(withFetch(), withInterceptors([authInterceptor])), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideEffects()]
 };
